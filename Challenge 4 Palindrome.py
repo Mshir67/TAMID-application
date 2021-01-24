2ad84c850f4d4944e0706555fcc818459be83ffc
Mshir67 <77933349+Mshir67@users.noreply.github.com>
2021-01-24 14:44:07 -0500

    Update Challenge 4 Palindrome.py

--- Challenge 4 Palindrome.py
+++ Challenge 4 Palindrome.py
@@ -3,11 +3,11 @@ string1 = input("Please enter a string: ")    # user inputted word
 string2 = string1[::-1]    # reverses string
 
 
-def Palindrome():
+def isPalindrome():
     if (string1 == string2):     # indicates palindrome 
         print(string1 + " is a palindrome")
     else:
         print(string1 + " is not a palindrome")
 
-Palindrome();    # calls function
+isPalindrome();    # calls function
  
